7c06ae4288efa8211faa35461e66ba2510f1dfae
Han <81735797+Han-Dong-Jin@users.noreply.github.com>
2025-05-02 17:51:34 +0900

    테스트용 setting 파일 추가

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,14 @@
-# test_main.py
-
 import pytest
 from unittest.mock import patch
+import lib.Config.config as config
 
 
-# main.py에서 run을 호출하는 main 함수를 테스트
-# 경로는 'main.py'에서 import한 위치 기준임
 @patch("lib.Crawling.run")
 def test_main_run_called(mock_run):
-    # 테스트 대상 함수 import
-    from main import main
+    # 테스트용 설정 파일 경로로 임시 변경
+    test_config_path = "test_settings.yaml"
+    with patch.object(config.Config, "_config_path", test_config_path):
+        from main import main
 
-    main()
-
-    # run() 함수가 1번 호출됐는지 확인
-    mock_run.assert_called_once()
+        main()
+        mock_run.assert_called_once()
