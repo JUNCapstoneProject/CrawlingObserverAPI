5ae57f33f54e4e397f9d9ee6fac869c127d04174
Han <81735797+Han-Dong-Jin@users.noreply.github.com>
2025-05-02 18:07:37 +0900

    테스트 오류 반영

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,14 @@
+import os
 import pytest
 from unittest.mock import patch
 import lib.Config.config as config
 
-# 설정 파일 경로를 테스트 설정으로 먼저 교체
-test_config_path = "test_settings.yaml"
+# 절대 경로로 지정
+test_config_path = os.path.abspath("test_settings.yaml")
 
-# 이걸 먼저 실행해줘야 합니다
 patcher = patch.object(config.Config, "_config_path", test_config_path)
 patcher.start()
 
-# 이제 main을 import (이 시점엔 이미 패치 적용됨)
 from main import main
 
 
@@ -19,5 +18,4 @@ def test_main_run_called(mock_run):
     mock_run.assert_called_once()
 
 
-# 테스트 끝난 후 패치 해제
 patcher.stop()
