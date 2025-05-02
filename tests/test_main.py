c31498fbef76a30c8df40317550df40ceef15c04
Han <81735797+Han-Dong-Jin@users.noreply.github.com>
2025-05-02 18:24:18 +0900

    테스트 오류 반영

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,20 +3,20 @@ import pytest
 from unittest.mock import patch
 import lib.Config.config as config
 
-# 절대 경로 지정 (파일이 있는 곳 기준)
+# 절대 경로 지정
 test_config_path = os.path.join(os.path.dirname(__file__), "test_settings.yaml")
 
-# 경로 패치 먼저
-patcher = patch.object(config.Config, "_config_path", test_config_path)
-patcher.start()
 
-from main import main  # 패치 후에 import!
+@pytest.fixture(autouse=True)
+def patch_config_path():
+    with patch.object(config.Config, "_config_path", test_config_path):
+        yield  # 테스트 실행
+        # 자동으로 patch 해제됨
 
 
 @patch("lib.Crawling.run")
 def test_main_run_called(mock_run):
+    from main import main  # patch 이후에 import 해야 반영됨
+
     main()
     mock_run.assert_called_once()
-
-
-patcher.stop()
